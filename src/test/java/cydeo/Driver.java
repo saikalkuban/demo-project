ac13856751d643f8f8c485fe5c295aab1b71bb8b
Saikal <saikal.cydeo.com>
2022-07-16 07:27:31 -0700

    trigger practice

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "52.90.101.317";
+                        String gridAddress = "44.202.236.89";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
@@ -44,6 +44,8 @@ public class Driver {
                         e.printStackTrace();
                     }
                     break;
+
+
 
                 case "remote-firefox":
 
